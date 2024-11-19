4820432b9bdeac187ef5ea50cc5bac457257f52e
ZhenyaSinitsyn <sinitsyn09@gmail.com>
2024-11-19 17:54:11 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,22 @@
+# # import name2 as n2
+# from name2 import *
+#
+# print('Hi, i am from Module 1')
+#
+# say_hi()
+# print(a)
+# print(b)
+
+import fake_math as f
+import true_math as t
+
+result1 = f.divide(69, 3)
+result2 = f.divide(3, 0)
+result3 = t.divide(49, 7)
+result4 = t.divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+
+
